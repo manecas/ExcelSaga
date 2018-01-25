fd58833e20117e9288b8bcfc96f51473ee4011c6
Luis <castrosoaresl@gmail.com>
2018-01-25 01:15:45 +0000

    First commit - Adding CardLayout

---
+++ src/view/RowHeaderRenderer.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JTable;
+import javax.swing.ListCellRenderer;
+import javax.swing.UIManager;
+import javax.swing.table.JTableHeader;
+
+/**
+ *
+ * @author Luis
+ */
+public class RowHeaderRenderer extends JLabel implements ListCellRenderer {
+
+    RowHeaderRenderer(JTable table) {
+        JTableHeader header = table.getTableHeader();
+        setOpaque(true);
+        setBorder(UIManager.getBorder("TableHeader.cellBorder"));
+        setHorizontalAlignment(CENTER);
+        setForeground(header.getForeground());
+        setBackground(header.getBackground());
+        setFont(header.getFont());
+  }
+    
+    @Override
+    public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+        setText((value == null) ? "" : value.toString());
+        return this;
+    }
+    
+}
